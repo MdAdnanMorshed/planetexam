544b8b39105eb3866503668d26e70d51c5624934
adnan morshed <adnan.starit118@gmail.com>
2021-05-24 00:44:26 +0600

    add to cart controller

--- lib/mvc/controllers/add_to_cart_controller.dart
+++ lib/mvc/controllers/add_to_cart_controller.dart
@@ -1,15 +1,55 @@
 
 import 'package:get/get.dart';
+import 'package:planetexam/mvc/models/models.dart';
 
 class AddToCartController extends GetxController{
+  var productCart = List<ProductCart>.empty(growable: true).obs;
 
+  double get totalPrice => productCart.fold(
+      0,
+          (sum, price) => sum = sum +
+          double.parse(price._productModel.basePrice) *
+              double.parse(price.pQty.toString()));
 
+  addToCart(NewProductModel _products){
+    for (var i = 0; i < productCart.length; i++) {
+      ProductCart temp = productCart[i];
 
+      if (temp._productModel.id == _products.id) {
+        productCart[i].pQty++;
+        return;
+      }
+    }
 
-  addToCart(){}
-  removeFromCart(){}
-  removeAllCart(){}
-  addToCartQtyIncrement(){}
-  addToCartQtyDecrement(){}
-  
+    ProductCart itemCart = ProductCart(_products, 1);
+    productCart.add(itemCart);
+  }
+
+  removeFromCart(int index){
+    productCart.removeAt(index);
+  }
+  removeAllCart(){
+    productCart.clear();
+  }
+  addToCartQtyIncrement( NewProductModel _products ,int index){
+    productCart[index].pQty++;
+  }
+  addToCartQtyDecrement(NewProductModel _products ,int index){
+    productCart[index].pQty--;
+  }
+}
+
+class ProductCart{
+  NewProductModel _productModel;
+  int pQty ;
+
+  ProductCart(this._productModel, this.pQty);
+
+  void setQty(int i) {
+    if (i < 1) {
+      pQty = i;
+    } else {
+      pQty = i;
+    }
+  }
 }
